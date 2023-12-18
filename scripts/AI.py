cf1c385857ceaea6895482c1a54509e83ec2adb6
PlexILIghT <liexa2024@gmail.com>
2023-12-18 10:23:29 +1000

    update AI

--- scripts/AI.py
+++ scripts/AI.py
@@ -3,3 +3,27 @@
 # Bot needs a player detection system.
 # Bot should be able to change animations.
 # rename for enemies
+
+
+class Bot:
+
+    def __init__(self, damage, health, speed):
+        self.damage = damage
+        self.health = health
+        self.speed = speed
+
+    def move(self, x, y):
+        pass
+    def look_for_player(self):
+        pass
+
+    def shooting(self):
+        pass
+
+    def dead(self):
+        pass
+
+    def lose_hp(self, other: int):
+        self.health -= int
+
+
